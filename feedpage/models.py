f4bf2afc2ad456d7f56a4faad1c19bd3b977c4a4
jenny5546 <jenny5546@naver.com>
2019-08-06 21:38:58 +0900

    jaeeun

---
+++ feedpage/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver   
+from taggit.managers import TaggableManager
+
+class Feed(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(User, null=True, on_delete= models.CASCADE) 
+    sunny_content =models.CharField(max_length=256)
+    cloudy_content= models.CharField(max_length=256)
+    rainy_content=models.CharField(max_length=256)
+    sunny_users = models.ManyToManyField(User, blank=True, related_name='feeds_sunny', through='Sunny')
+    cloudy_users = models.ManyToManyField(User, blank=True, related_name='feeds_cloudy', through='Cloudy')
+    rainy_users = models.ManyToManyField(User, blank=True, related_name='feeds_rainy', through='Rainy')
+    photo = models.ImageField(blank=True, upload_to='feed_photos')
+    tags = TaggableManager()
+
+    def update_date(self):
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Profile(models.Model):   
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def __str__(self):  
+        return self.nickname
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):  
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):  
+    instance.profile.save()
+
+class FeedComment(models.Model):
+    content = models.TextField()
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, null=True, on_delete= models.CASCADE) 
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Sunny(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Cloudy(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Rainy(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
---
+++ feedpage/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Profile, Feed, FeedComment, Sunny, Cloudy, Rainy
+from django.contrib.auth.models import User
+
+def index(request):
+    if request.method == 'GET': 
+        feeds = Feed.objects.all()
+        return render(request, 'feedpage/index.html', {'feeds': feeds})
+    elif request.method == 'POST': 
+        title = request.POST['title']
+        content = request.POST['content']
+        photo =  request.FILES.get('photo', False)
+        sunny_content =request.POST['sunny_content']
+        cloudy_content= request.POST['cloudy_content']
+        rainy_content=request.POST['rainy_content']
+        Feed.objects.create(title=title,content=content,author=request.user,photo=photo, sunny_content=sunny_content, cloudy_content=cloudy_content, rainy_content=rainy_content)
+        return redirect('/home')
+
+def new(request):
+    return render(request, 'feedpage/new.html')
+
+def show(request, id):
+    if request.method == 'GET': # show
+        feed = Feed.objects.get(id=id)
+        return render(request, 'feedpage/show.html', {'feed': feed})
+    elif request.method == 'POST': # create
+        title = request.POST['title']
+        content = request.POST['content']
+        feed = Feed.objects.get(id=id)
+        feed.title= title
+        feed.content=content
+        feed.save()
+        feed.update_date()
+        return redirect('/home/'+str(id))
+
+def delete(request, id):
+    feed = Feed.objects.get(id=id)
+    feed.delete()
+    return redirect('/home')
+
+def edit(request, id):
+    feed = Feed.objects.get(id=id)
+    return render(request, 'feedpage/edit.html', {'feed':feed})
+
+def create_comment(request, id):
+    content = request.POST['content']
+    FeedComment.objects.create(feed_id=id, content=content, author = request.user)
+    return redirect('/home')
+
+def delete_comment(request, id, cid):
+    c = FeedComment.objects.get(id=cid)
+    c.delete()
+    return redirect('/home')
+
+def feed_sunny(request, pk):
+    feed = Feed.objects.get(id = pk)
+    sunny_list = feed.sunny_set.filter(user_id = request.user.id)
+    if sunny_list.count() > 0:
+        feed.sunny_set.get(user_id = request.user.id).delete()
+    else:
+        Sunny.objects.create(user_id = request.user.id, feed_id = feed.id)
+    return redirect ('/home')
+
+def feed_cloudy(request, pk):
+    feed = Feed.objects.get(id = pk)
+    cloudy_list = feed.cloudy_set.filter(user_id = request.user.id)
+    if cloudy_list.count() > 0:
+        feed.cloudy_set.get(user_id = request.user.id).delete()
+    else:
+        Cloudy.objects.create(user_id = request.user.id, feed_id = feed.id)
+    return redirect ('/home')
+
+def feed_rainy(request, pk):
+    feed = Feed.objects.get(id = pk)
+    rainy_list = feed.rainy_set.filter(user_id = request.user.id)
+    if rainy_list.count() > 0:
+        feed.rainy_set.get(user_id = request.user.id).delete()
+    else:
+        Rainy.objects.create(user_id = request.user.id, feed_id = feed.id)
+    return redirect ('/home')
+
+def userinfo(request):
+        c_user= request.user
+        c_profile=Profile.objects.get(user=c_user)
+        context= {
+                'id' : c_user.id,
+        }
+        return render(request, 'feedpage/mypage.html', context=context)
