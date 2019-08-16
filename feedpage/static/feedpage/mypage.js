61731b52c6ff58c86bc68b27816bd52e78d80727
suuuum36 <lsm3191@snu.ac.kr>
2019-08-16 23:50:35 +0900

    글쓰기 중

--- feedpage/static/feedpage/mypage.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction() {
-    var x = document.getElementById("forecast_list");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
