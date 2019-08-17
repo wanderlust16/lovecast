52af585c5270ad8fc128eff8a85a85d40730af56
suuuum36 <lsm3191@snu.ac.kr>
2019-08-17 23:44:23 +0900

    edit 완성

--- feedpage/models.py
+++ feedpage/models.py
@@ -25,7 +25,7 @@ class Feed(models.Model):
     cloudy_users = models.ManyToManyField(User, blank=True, related_name='feeds_cloudy', through='Cloudy')
     rainy_users = models.ManyToManyField(User, blank=True, related_name='feeds_rainy', through='Rainy')
     nickname=models.CharField(max_length=200, blank=True)
-    feed_photos = models.ManyToManyField(Photos, null=True, related_name='feed_photo')
+    feed_photos = models.ManyToManyField(Photos, related_name='feed_photo')
     hashtag_str=models.TextField(blank=True)
     result= models.CharField(default='not confirmed', max_length=200) #결과 확정. 
 
@@ -35,6 +35,13 @@ class Feed(models.Model):
 
     def __str__(self):
         return self.title
+
+class Notification(models.Model):
+    title= models.CharField(max_length=256)
+    message= models.TextField()
+    viewed= models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
 
 class Profile(models.Model):   
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
@@ -97,9 +104,5 @@ class CommentDislike(models.Model):
     comment = models.ForeignKey(FeedComment, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
-class Notification(models.Model):
-    title= models.CharField(max_length=256)
-    message= models.TextField()
-    viewed= models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
